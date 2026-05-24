3fb0f639b219e9420ea8672255e5b899ac753aba
abgferro <abg.distribuidora.ltda@gmail.com>
2026-05-24 02:25:40 -0300

    add missing pages

---
+++ src/pages/CalendarioPage.jsx
@@ -0,0 +1,10 @@
+function CalendarioPage() {
+  return (
+    <div className="container section">
+      <h1>CalendarioPage</h1>
+      <p>Em breve...</p>
+    </div>
+  )
+}
+
+export default CalendarioPage
---
+++ src/pages/ConfiguracoesPage.jsx
@@ -0,0 +1,10 @@
+function ConfiguracoesPage() {
+  return (
+    <div className="container section">
+      <h1>ConfiguracoesPage</h1>
+      <p>Em breve...</p>
+    </div>
+  )
+}
+
+export default ConfiguracoesPage
